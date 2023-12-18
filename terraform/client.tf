24c4e2fb54258dd2f58306fa14c0a82a4f2cf0e5
Lucatony Raudales <tony.raudales@scrummers.co>
2023-12-18 15:33:47 -0600

    a

--- terraform/client.tf
+++ terraform/client.tf
@@ -11,3 +11,71 @@ resource "kubernetes_pod" "cliente_pod" {
     }
   }
 }
+resource "kubernetes_deployment" "web_app" {
+  metadata {
+    name = "web-app"
+
+    labels = {
+      webappname = "web-app"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        webappname = "web-app"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          webappname = "web-app"
+        }
+      }
+
+      spec {
+        container {
+          name    = "web-app"
+          image   = "nginx"
+
+          command = ["/bin/sh", "-c", "echo 'welcome to my web app!' > /usr/share/nginx/html/index.html && nginx -g 'daemon off;'"]
+        }
+
+        dns_config {
+          option {
+            name  = "ndots"
+            value = "2"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "web_app" {
+  metadata {
+    name = "web-app"
+
+    labels = {
+      webappname = "web-app"
+    }
+  }
+
+  spec {
+    selector = {
+      webappname = "web-app"
+    }
+
+    port {
+      name        = "http"
+      port        = 80
+      protocol    = "TCP"
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
